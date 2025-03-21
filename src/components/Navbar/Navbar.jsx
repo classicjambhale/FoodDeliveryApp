4e964cd932da2fa6fb2043ff39d1679617731a00
classicjambhale <classicj8066@gmail.com>
2025-03-21 12:22:04 +0530

    Added code for dot

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import "./Navbar.css"
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
+import { StoreContext } from '../../context/StoreContext';
 
 function Navbar({setShowLogin}) {
   const [menu,setMenu] = useState("menu");
+
+  // to show / hide cart dot
+  const {getTotalCartAmount} = useContext(StoreContext)
 
   return (
     <div className='navbar'>
@@ -19,7 +23,7 @@ function Navbar({setShowLogin}) {
           <img src={assets.search_icon} alt="" />
           <div className="navbar-search-icon">
             <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
-            <div className="dot"></div>
+            <div className={getTotalCartAmount()===0?"":"dot"}></div>
           </div>
           {/* when click on login then popup will comes */}
           <button onClick={()=>setShowLogin(true)}>sign in</button>
