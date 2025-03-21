eabadc88186a50ca19950e37fe5c90945f74d901
classicjambhale <classicj8066@gmail.com>
2025-03-21 12:51:40 +0530

    Complete code

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,10 +1,15 @@
 import React, { useContext } from 'react'
 import './Cart.css'
 import { StoreContext } from '../../context/StoreContext'
+import { useNavigate } from 'react-router-dom';
 
 function Cart() {
 
   const {cartItems,food_list,removeFromCart,getTotalCartAmount} = useContext(StoreContext);
+
+  // To place order after clicking on proceed to checkout
+  const navigate = useNavigate()
+
   return (
     <div className='cart'>
       <div className="cart-items">
@@ -50,15 +55,15 @@ function Cart() {
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>${2}</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount()+2}</b>
+              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
             </div>
           </div>
-          <button>PROCEED TO CHECKOUT</button>
+          <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
         </div>
         <div className="cart-promocode">
 
